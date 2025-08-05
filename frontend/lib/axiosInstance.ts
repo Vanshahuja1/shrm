21274d1c86c12ee77425091042157047d19caf36
Shikhar Pandey <shikhar2003.pandey@gmail.com>
2025-08-05 12:35:38 +0530

    CHANGE

--- frontend/lib/axiosInstance.ts
+++ frontend/lib/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
-  // baseURL: "https://shrm-backend.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://shrm-backend.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
